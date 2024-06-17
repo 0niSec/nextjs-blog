530499c8d2fda045db8d7463edf7437e479fe370
Nick <nconkl1@outlook.com>
2024-06-17 15:29:25 -0400

    updated CSP headers

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,30 @@ if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
   await build({ watch: isDev, clean: !isDev });
 }
 
+const cspHeader = `
+  default-src: 'self';
+  script-src: 'self' 'unsafe-inline' 'unsafe-eval' https://clerk.accounts.dev https://civil-longhorn-92.clerk.accounts.dev;
+  connect-src: 'self' https://civil-longhorn-92.clerk.accounts.dev https://clerk.accounts.dev;
+  img-src: 'self' https://img.clerk.com;
+  worker-src: 'self' blob:;
+  style-src: 'self' 'unsafe-inline';
+`;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
